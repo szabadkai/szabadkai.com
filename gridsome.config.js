6c5cebfaff63477878321d0d82cfa82908f0d6d7
Levente Szabadkai <levente.szabadkai@goto.com>
2022-08-08 09:10:21 +0200

    fix url

--- gridsome.config.js
+++ gridsome.config.js
@@ -2,40 +2,40 @@
 // Learn more: https://gridsome.org/docs/config
 
 module.exports = {
-  siteName: "Portfolio",
-  siteUrl: `https://www.itsnwa.com`,
-  host: "0.0.0.0",
-  titleTemplate: "%s - NWA",
-  siteDescription: "Creative technologist",
-  plugins: [
-    {
-      use: "@gridsome/source-filesystem",
-      options: {
-        path: "projects/**/*.md",
-        typeName: "ProjectPost",
-        resolveAbsolutePaths: true,
+    siteName: "Portfolio",
+    siteUrl: `https://www.szabadkai.com`,
+    host: "0.0.0.0",
+    titleTemplate: "%s - NWA",
+    siteDescription: "Creative technologist",
+    plugins: [
+        {
+            use: "@gridsome/source-filesystem",
+            options: {
+                path: "projects/**/*.md",
+                typeName: "ProjectPost",
+                resolveAbsolutePaths: true,
+                remark: {
+                    externalLinksTarget: "_blank",
+                    externalLinksRel: ["nofollow", "noopener", "noreferrer"],
+                },
+            },
+        },
+        {
+            use: "@gridsome/source-filesystem",
+            options: {
+                path: "journal/**/*.md",
+                typeName: "JournalPost",
+                resolveAbsolutePaths: true,
+                remark: {
+                    externalLinksTarget: "_blank",
+                    externalLinksRel: ["nofollow", "noopener", "noreferrer"],
+                },
+            },
+        },
+    ],
+    transformers: {
         remark: {
-          externalLinksTarget: "_blank",
-          externalLinksRel: ["nofollow", "noopener", "noreferrer"]
-        }
-      }
+            plugins: ["@gridsome/remark-prismjs"],
+        },
     },
-    {
-      use: "@gridsome/source-filesystem",
-      options: {
-        path: "journal/**/*.md",
-        typeName: "JournalPost",
-        resolveAbsolutePaths: true,
-        remark: {
-          externalLinksTarget: "_blank",
-          externalLinksRel: ["nofollow", "noopener", "noreferrer"]
-        }
-      }
-    }
-  ],
-  transformers: {
-    remark: {
-      plugins: ["@gridsome/remark-prismjs"]
-    }
-  }
 };
